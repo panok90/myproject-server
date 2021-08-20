4087af8bc60b2b855fe6deea4e3604dab309d8df
panok90 <cool.cyclon@yandex.ru>
2021-08-20 18:34:09 +0300

    Added lesson 3

--- products/models.py
+++ products/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class ProductCategory(models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=256, unique=True)
@@ -11,3 +13,5 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.name} | {self.category.name}'
