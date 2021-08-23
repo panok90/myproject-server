5548e0b1693c2a89f79ceb7bacef973ab224ec54
panok90 <cool.cyclon@yandex.ru>
2021-08-23 22:04:29 +0300

    fixed models.py

--- products/models.py
+++ products/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
-class ProductCategory(models.Model):
+
+class ProductCategory (models.Model):
     name = models.CharField(max_length=64, unique=True)
     description = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return self.name
 
-class Product(models.Model):
+
+class Product (models.Model):
     name = models.CharField(max_length=256, unique=True)
     image = models.ImageField(upload_to='products_images', blank=True)
     description = models.CharField(max_length=64, blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
     def __str__(self):
         return f'{self.name} | {self.category.name}'
