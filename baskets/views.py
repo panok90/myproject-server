41d56ec2ffc4c2501e614b77438d557bb0b8d27d
panok90 <cool.cyclon@yandex.ru>
2021-08-30 01:04:29 +0300

    Added lesson 6

--- baskets/views.py
+++ baskets/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 from baskets.models import Basket
 from products.models import Product
@@ -25,3 +27,21 @@ def basket_remove(request, id):
     basket = Basket.objects.get(id=id)
     basket.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_edit(request, id, quantity):
+    if request.is_ajax():
+        basket = Basket.objects.get(id=id)
+        if quantity > 0:
+            basket.quantity = quantity
+            basket.save()
+        else:
+            basket.delete()
+        user = request.user
+        baskets = Basket.objects.filter(user=user)
+        context = {'baskets': baskets,
+                   'total_quantity': Basket.total_quantity(user),
+                   'total_sum': Basket.total_sum(user),
+                   }
+        result = render_to_string('baskets/baskets.html', context)
+        return JsonResponse({'result': result})
