d2878391908ea9c6c2329b4d05742b80b02a7b17
panok90 <cool.cyclon@yandex.ru>
2021-09-01 23:09:48 +0300

    created controller  for create users

---
+++ admins/forms.py
@@ -0,0 +1,11 @@
+from users.forms import UserRegistrationForm
+from users.forms import User
+from django import forms
+
+
+class UserAdminRegistrationForm(UserRegistrationForm):
+    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'image', 'first_name', 'last_name', 'password1', 'password1')
--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 def index(request):
@@ -17,3 +19,16 @@ def admin_users(request):
         'users': User.objects.all(),
     }
     return render(request, 'admins/admin-users.html', context)
+
+
+#Create
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admins:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'GeekShop- Создание пользователя', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
