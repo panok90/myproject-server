7bc7fc8dc140855cfdbde170ae6389c7228ea384
panok90 <cool.cyclon@yandex.ru>
2021-09-07 10:07:40 +0300

    Added ListView and CreateView

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse_lazy, reverse
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
@@ -15,27 +17,17 @@ def index(request):
 
 
 #Read
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    context = {
-        'title': 'GeekShop - Пользователи',
-        'users': User.objects.all(),
-    }
-    return render(request, 'admins/admin-users.html', context)
+class UserListView(ListView):
+    model = User
+    template_name = 'admins/admin-users.html'
 
 
 #Create
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {'title': 'GeekShop- Создание пользователя', 'form': form}
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users')
 
 
 #Update
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from products.models import Product, ProductCategory
 
 
@@ -9,8 +11,15 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
     context = {'title': 'GeekShop - Каталог', 'categories': ProductCategory.objects.all()}
     products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
-    context['products'] = products
+    paginator = Paginator(products, per_page=3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
